675324bd65ffb2dedc2d1ace588ed04d78e7a3d8
Babu Natarajan <babunatarajan6@gmail.com>
2020-12-23 13:32:52 -0500

    Create docker-install-centos7.sh

---
+++ docker-install-centos7.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Please run this using sudoer account or root."
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
